1dec99aab48f1b6ed3bfe329845bcc05c2585016
Shane Hughes <shughes@student.yosemite.edu>
2015-05-11 06:42:19 -0700

    stuff

--- Autolayout_193p/ViewController.swift
+++ Autolayout_193p/ViewController.swift
@@ -8,18 +8,78 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
+    var loggedInUser: User? { didSet { updateUI() }}
+    var secure: Bool = false { didSet { updateUI() }}
+    
+    
+    @IBOutlet weak var loginField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var companyLabel: UILabel!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+  
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateUI()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    
+    
+    private func updateUI(){
+        passwordField.secureTextEntry = secure
+        passwordLabel.text = secure ? "Secured Password" : "Password"
+        //OPTIONAL CHAINING
+        nameLabel.text = loggedInUser?.name
+        companyLabel.text = loggedInUser?.company
+        imageView.image = loggedInUser?.image
     }
+    
+    
+    
+    
+    @IBAction func toggleSecurity() {
+        secure = !secure
+    }
+    
+    
+    
+    
+    
+    @IBAction func login() {
+        loggedInUser = User.login(loginField.text ?? "", password: passwordField.text ?? "")
+        }
+    
+    
+    
+    
+    
+    
+}// ViewController
 
-
+extension User
+{
+    var image: UIImage? {
+        if let image = UIImage(named: login ){
+            return image
+        } else {
+            return UIImage(named: "unknown_user")
+        }
+    }
 }
+
+
+
+
+
+
 
